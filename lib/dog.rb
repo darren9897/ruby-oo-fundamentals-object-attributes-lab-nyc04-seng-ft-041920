db1ebdb1b7ebf275b47c16a0e2a01d9603b81274
Darren Koomson <darrenkoomson17@gmail.com>
2020-05-12 20:23:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog 
-    def initialize(breed, name)
-      @breed = breed
-      @name = name
-    end
+    # def initialize(breed, name)
+    #   @breed = breed
+    #   @name = name
+    # end
     
     def name 
       @name
